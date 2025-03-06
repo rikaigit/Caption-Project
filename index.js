cb8d80d0e414936f9f9a43f4d966cbb484ee9679
Arindam Sen <CW231212@CW.local>
2025-03-06 13:34:23 +0530

    check the post description using log

--- index.js
+++ index.js
@@ -43,6 +43,7 @@ app.post("/submit", (req, res) => {
     let postDescription = req.body.description;
     let publishedDate = new Date().toLocaleDateString("en-GB");
     console.log(postTitles);
+    console.log(postDescription);
     allPosts.push({postTitles, publishedDate });
     res.redirect("/view"); //This is for redirect the ejs file on server to avoid the re-submission.
 });
